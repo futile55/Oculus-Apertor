0ed03807bd424bb4e03c1909c3ef7fd1a3edc733
Rohan Chhabra <rohan23chhabra@gmail.com>
2018-03-17 06:39:57 +0530

    No more permission problems for GPS.

--- app/src/main/java/org/waoss/oculus/apertor/DrivingActivity.java
+++ app/src/main/java/org/waoss/oculus/apertor/DrivingActivity.java
@@ -1,9 +1,6 @@
 package org.waoss.oculus.apertor;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.media.AudioManager;
@@ -12,7 +9,6 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -48,15 +44,7 @@ public class DrivingActivity extends AppCompatActivity implements EyesClosedList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driving);
         requestPermissions();
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        locationManager
-                .requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE,
-                        new OculusLocationListener(new Runnable() {
-                            @Override
-                            public void run() {
-                                runOnUiThread(PLAY_BEEP_SOUND);
-                            }
-                        }), Looper.getMainLooper());
+
         cameraSourcePreview = findViewById(R.id.preview);
         root = findViewById(R.id.root);
         defaultCameraOperator = new DefaultCameraOperator(this, TAG, this,
@@ -70,32 +58,22 @@ public class DrivingActivity extends AppCompatActivity implements EyesClosedList
         } else {
             requestPermissions();
         }
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager
+                .requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE,
+                        new OculusLocationListener(new Runnable() {
+                            @Override
+                            public void run() {
+                                runOnUiThread(PLAY_BEEP_SOUND);
+                            }
+                        }), Looper.getMainLooper());
     }
 
     private void requestPermissions() {
         Log.w(TAG, "Camera is not granted permission. Requesting for permission");
 
         final String permissions[] = new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION};
-
-        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.CAMERA) ||
-                !ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
-            return;
-        }
-
-        final Activity thisActivity = this;
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ActivityCompat.requestPermissions(thisActivity, permissions,
-                        RC_HANDLE_CAMERA_PERM);
-            }
-        };
-        Snackbar.make(root, "Please give permission to access the camera and GPS", Snackbar.LENGTH_INDEFINITE)
-                .setAction("OK", listener)
-                .show();
-
+        ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
     }
 
     @Override
@@ -138,33 +116,16 @@ public class DrivingActivity extends AppCompatActivity implements EyesClosedList
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
-        if (requestCode != RC_HANDLE_CAMERA_PERM) {
-            Log.d(TAG, "Got unexpected permission result: " + requestCode);
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            return;
+        switch (requestCode) {
+            case RC_HANDLE_CAMERA_PERM:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Log.e(TAG, "LOL");
+                }
+                break;
         }
-
-        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-            Log.d(TAG, "Camera permission granted - initialize the camera source");
-            defaultCameraOperator.createCameraSource();
-            return;
-        }
-
-        Log.e(TAG, "Permission not granted: results len = " + grantResults.length +
-                " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
-
-        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                finish();
-            }
-        };
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Face Tracker sample")
-                .setMessage("NO permission camera or probs gps. App sad. :( :(")
-                .setPositiveButton("ok", listener)
-                .show();
     }
 
     public void onFlipButtonClicked(final View view) {
