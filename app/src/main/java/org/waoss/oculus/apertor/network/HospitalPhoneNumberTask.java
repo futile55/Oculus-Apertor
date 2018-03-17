37c4e8d529df64556b4d57a99d7475e8d383a4f5
Rohan Chhabra <rohan23chhabra@gmail.com>
2018-03-18 01:22:24 +0530

    Network operation done in ASyncTask.

--- app/src/main/java/org/waoss/oculus/apertor/network/HospitalPhoneNumberTask.java
+++ app/src/main/java/org/waoss/oculus/apertor/network/HospitalPhoneNumberTask.java
@@ -1,12 +1,86 @@
 package org.waoss.oculus.apertor.network;
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.google.android.gms.location.places.*;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import okhttp3.*;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
 
 public class HospitalPhoneNumberTask extends AsyncTask<Void, Void, List<CharSequence>> {
+    public static final String TAG = HospitalPhoneNumberTask.class.getSimpleName();
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private GeoDataClient geoDataClient;
+    private OkHttpClient client = new OkHttpClient();
+    private Context context;
+
+    public HospitalPhoneNumberTask(Context context) {
+        this.context = context;
+    }
+
     @Override
     protected List<CharSequence> doInBackground(final Void... voids) {
+        final List<CharSequence> phoneNumbers = new ArrayList<>();
+        geoDataClient = Places.getGeoDataClient(context, null);
+        HttpUrl.Builder httpUrlBuilder = HttpUrl
+                .parse("https://maps.googleapis.com/maps/api/place/nearbysearch/json").newBuilder();
+        httpUrlBuilder.addQueryParameter("location", "25.4263508,81.7732333");
+        httpUrlBuilder.addQueryParameter("radius", "1000");
+        httpUrlBuilder.addQueryParameter("types", "hospital");
+        httpUrlBuilder.addQueryParameter("key", "AIzaSyDDyx4sdmsirBzdlmvK6v2-Hmcj7VzosGc");
+        HttpUrl httpUrl = httpUrlBuilder.build();
+        Request request = new Request.Builder()
+                .url(httpUrl)
+                .get()
+                .build();
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        if (response != null) {
+            try {
+                JsonNode jsonNode = objectMapper.readTree(response.body().byteStream());
+                ArrayNode results = (ArrayNode) jsonNode.get("results");
+                Iterator<JsonNode> resultsIterator = results.elements();
+                while (resultsIterator.hasNext()) {
+                    JsonNode nextNode = resultsIterator.next();
+                    String placeId = nextNode.get("place_id").asText();
+                    geoDataClient.getPlaceById(placeId)
+                            .addOnCompleteListener(new OnCompleteListener<PlaceBufferResponse>() {
+                                @Override
+                                public void onComplete(@NonNull Task<PlaceBufferResponse> task) {
+                                    if (task.isSuccessful()) {
+                                        PlaceBufferResponse places = task.getResult();
+                                        Place myPlace = places.get(0);
+                                        phoneNumbers.add(myPlace.getPhoneNumber());
+                                        Log.i(TAG, "Place found: " + myPlace.getName());
+                                        places.release();
+                                    } else {
+                                        Log.e(TAG, "Place not found.");
+                                    }
+                                }
+                            });
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return phoneNumbers;
+    }
+
+    @Override
+    protected void onPostExecute(final List<CharSequence> charSequences) {
+        super.onPostExecute(charSequences);
     }
 }
--- app/src/main/java/org/waoss/oculus/apertor/service/CrashService.java
+++ app/src/main/java/org/waoss/oculus/apertor/service/CrashService.java
@@ -7,20 +7,14 @@ import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.*;
 import android.os.Process;
-import android.support.annotation.NonNull;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.google.android.gms.location.places.*;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import okhttp3.*;
+import org.waoss.oculus.apertor.network.HospitalPhoneNumberTask;
 
-import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class CrashService extends Service {
 
@@ -70,13 +64,10 @@ public class CrashService extends Service {
     private final class ServiceHandler extends Handler {
         private static final String CRASH_TEXT = "Kindly ignore the message!";
 
-        private ObjectMapper objectMapper = new ObjectMapper();
-        private GeoDataClient geoDataClient;
-        private OkHttpClient client = new OkHttpClient();
 
         public ServiceHandler(Looper looper) {
             super(looper);
-            geoDataClient = Places.getGeoDataClient(getApplicationContext(), null);
+
         }
 
         @Override
@@ -99,14 +90,18 @@ public class CrashService extends Service {
                     float gZ = z / 9.8f;
 
                     double gForce = Math.sqrt(gX * gX + gY * gY + gZ * gZ);
-                    if (gForce > 1.01) {
+                    if (gForce > 35) {
                         final ToneGenerator toneGenerator = new ToneGenerator(AudioManager.STREAM_NOTIFICATION,
                                 ToneGenerator.MAX_VOLUME);
                         toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP);
                         toneGenerator.release();
                         sendSMS();
                         if (!hasFilledPhoneNumbers) {
-                            fillPhoneNumbers();
+                            try {
+                                fillPhoneNumbers();
+                            } catch (ExecutionException | InterruptedException e) {
+                                e.printStackTrace();
+                            }
                         }
                         sendSMSToNearbyHospitals();
                     }
@@ -128,54 +123,11 @@ public class CrashService extends Service {
                     }
                 }
 
-                private void fillPhoneNumbers() {
-                    HttpUrl.Builder httpUrlBuilder = HttpUrl
-                            .parse("https://maps.googleapis.com/maps/api/place/nearbysearch/json").newBuilder();
-                    httpUrlBuilder.addQueryParameter("location", "25.4263508,81.7732333");
-                    httpUrlBuilder.addQueryParameter("radius", "1000");
-                    httpUrlBuilder.addQueryParameter("types", "hospital");
-                    httpUrlBuilder.addQueryParameter("key", "AIzaSyDDyx4sdmsirBzdlmvK6v2-Hmcj7VzosGc");
-                    HttpUrl httpUrl = httpUrlBuilder.build();
-                    Request request = new Request.Builder()
-                            .url(httpUrl)
-                            .get()
-                            .build();
-                    Response response = null;
-                    try {
-                        response = client.newCall(request).execute();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    if (response != null) {
-                        try {
-                            JsonNode jsonNode = objectMapper.readTree(response.body().byteStream());
-                            ArrayNode results = (ArrayNode) jsonNode.get("results");
-                            Iterator<JsonNode> resultsIterator = results.elements();
-                            while (resultsIterator.hasNext()) {
-                                JsonNode nextNode = resultsIterator.next();
-                                String placeId = nextNode.get("place_id").asText();
-                                geoDataClient.getPlaceById(placeId)
-                                        .addOnCompleteListener(new OnCompleteListener<PlaceBufferResponse>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<PlaceBufferResponse> task) {
-                                                if (task.isSuccessful()) {
-                                                    PlaceBufferResponse places = task.getResult();
-                                                    Place myPlace = places.get(0);
-                                                    phoneNumbers.add(myPlace.getPhoneNumber());
-                                                    Log.i(TAG, "Place found: " + myPlace.getName());
-                                                    places.release();
-                                                } else {
-                                                    Log.e(TAG, "Place not found.");
-                                                }
-                                            }
-                                        });
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        hasFilledPhoneNumbers = true;
-                    }
+                private void fillPhoneNumbers() throws ExecutionException, InterruptedException {
+                    HospitalPhoneNumberTask hospitalPhoneNumberTask = new HospitalPhoneNumberTask(
+                            getApplicationContext());
+                    phoneNumbers = hospitalPhoneNumberTask.execute().get();
+                    hasFilledPhoneNumbers = true;
                 }
 
                 private void sendSMSToNearbyHospitals() {
