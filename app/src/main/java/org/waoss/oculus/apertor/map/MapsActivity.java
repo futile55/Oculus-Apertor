67edcaae7f0d0fb6c8bcd9cddcc2de308626c472
Rohan Chhabra <rohan23chhabra@gmail.com>
2018-03-17 13:07:46 +0530

    Still adding map support.

--- app/src/main/java/org/waoss/oculus/apertor/map/MapsActivity.java
+++ app/src/main/java/org/waoss/oculus/apertor/map/MapsActivity.java
@@ -1,54 +1,106 @@
 package org.waoss.oculus.apertor.map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.*;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import org.waoss.oculus.apertor.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    public static final float LOCATION_REFRESH_DISTANCE = 1;
-    public static final long LOCATION_REFRESH_TIME = 2000;
-
-    private GoogleMap map;
-    private MapLocationListener mapLocationListener;
-    private Handler handler;
-    private Runnable updateMap = new Runnable() {
-        @Override
-        public void run() {
-            Location currentLocation = mapLocationListener.getCurrentLocation();
-            LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-            map.addMarker(new MarkerOptions().position(currentLatLng).title("Marker"));
-            map.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
-            handler.postDelayed(updateMap, 5000);
-        }
-    };
+    public static final String TAG = MapsActivity.class.getSimpleName();
+    public static final LatLng DEFAULT_LOCATION = new LatLng(23.33, 23.33);
+    private static final float DEFAULT_ZOOM = 15;
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private GoogleMap googleMap;
+    private GeoDataClient geoDataClient;
+    private PlaceDetectionClient placeDetectionClient;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private boolean isLocationPermissionGranted;
+    private Location lastKnownLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        geoDataClient = Places.getGeoDataClient(this, null);
+        placeDetectionClient = Places.getPlaceDetectionClient(this, null);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        handler = new Handler();
+    }
+
+    @Override
+    public void onSaveInstanceState(final Bundle outState) {
+        if (googleMap != null) {
+            outState.putParcelable(KEY_CAMERA_POSITION, googleMap.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION, lastKnownLocation);
+        }
+        super.onSaveInstanceState(outState);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-        mapLocationListener = new MapLocationListener(map);
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        locationManager
-                .requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE,
-                        mapLocationListener);
-        handler.postDelayed(updateMap, 500);
+        this.googleMap = googleMap;
+        getLocationPermission();
+        updateLocationUI();
+        getDeviceLocation();
     }
+
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            isLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private void updateLocationUI() {
+
+    }
+
+    private void getDeviceLocation() {
+        try {
+            if (isLocationPermissionGranted) {
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull final Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            lastKnownLocation = task.getResult();
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude()),
+                                    DEFAULT_ZOOM
+                            ));
+                        } else {
+                            Log.d(TAG, "Using default location");
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, DEFAULT_ZOOM));
+                            googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "{}", e);
+        }
+    }
+
 
 }
